ba6eed5e59ac65df1a01465b825bc1c635847e19
Ismael Reckziegel <ismaelreck@gmail.com>
2024-01-08 11:51:59 -0300

    Mudando background na viewController (registerViewController não teve Stage)

--- galleryStoryBoard_Swift/ViewController.swift
+++ galleryStoryBoard_Swift/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-            // novo teste
+        view.backgroundColor = UIColor.blue
         
     }
 
