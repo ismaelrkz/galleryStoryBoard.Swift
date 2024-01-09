9e55f7708b7bebb9e2f4e0c00e88b34ab176b936
Ismael Reckziegel <ismaelreck@gmail.com>
2024-01-09 09:37:52 -0300

    tabBarController adicionado no projeto

---
+++ galleryStoryBoard_Swift/VC/TabBarViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TabBarViewController.swift
+//  galleryStoryBoard_Swift
+//
+//  Created by Ismael Reckziegel on 08/01/24.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}
